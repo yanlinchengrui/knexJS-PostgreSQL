e7161e68a5bd0cc7c088b24d76e5d2fd066d3a28
yanlinchengrui <yanlinchengrui@hotmail.com>
2019-06-18 20:23:35 +0000

    fix connection settings

--- psql_codes/connection.js
+++ psql_codes/connection.js
@@ -1,5 +1,5 @@
 const pg = require("pg");
-const settings = require("./settings"); // settings.json
+const settings = require("../settings"); // settings.json
 
 const client = new pg.Client({
   user     : settings.user,
